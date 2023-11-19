d8e64113a647a30d4ec9bb46e1748ba053ac28d5
Anthony Cabulang <zyxanthony15@gmail.com>
2023-11-19 21:47:04 +0800

    Adding the summ and printing the numbers from 1-10

--- conditional-and-iterations/for-loop.js
+++ conditional-and-iterations/for-loop.js
@@ -2,3 +2,15 @@ document.write("<p>Print numbers from 1 to 100 </p>");
     for (i=0; i<=100; i++) {
         document.write(i + " ");
     }
+
+document.write("<p>Print the sum of all even numbers from 1-10</p>")
+    let num = 10;
+    let sum = 0;
+    document.write("<p>Printing numbers from 1 to 10</p>");
+    for (i = 0; i <= num; i++) {
+        document.write(" " + i);
+       if (i % 2 == 0) {
+           sum = sum + i;
+        }
+    }
+    document.write("<br><br><p>The sum of even numbers from 1 to 10: </p>" + sum);

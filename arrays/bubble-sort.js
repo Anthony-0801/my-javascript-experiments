fc0fcd18e53ecd885d041e81f8d8d26ef17df143
Anthony Cabulang <zyxanthony15@gmail.com>
2023-11-19 20:06:13 +0800

    Finishing the bubble sort

--- arrays/bubble-sort.js
+++ arrays/bubble-sort.js
@@ -1,8 +1,25 @@
 let arrangeMe = [10, 32, 402, 21, 11, 103, 23, 1, 5, 95];
-let i, j, temp;
-
-document.write("<p> Printing out the numbers without arranging </p>"); 
+let i;
+document.write("<p> 1. Printing out the numbers without arranging </p>"); 
 for (i = 0; i < arrangeMe.length; i++) {
     document.write(arrangeMe[i] + ",  ");
-
 }
+
+document.write("<p> 2. Printing out the array in ascending order using a bubble sort function</p>");
+const bubbleSort = (arr) => {
+let swapped;
+do {
+    swapped = false;
+    for(let a = 0; a < arr.length - 1; a++) {
+        if (arr[a] > arr[a + 1]) {
+            let temp = arr[a];
+            arr[a] = arr[a + 1];
+            arr[a + 1] = temp;
+            swapped = true;
+        }
+    }
+} while (swapped);
+return arr;
+};
+
+document.write(bubbleSort(arrangeMe));

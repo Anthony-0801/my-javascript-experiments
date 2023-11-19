31f9ae9d1db87bbee46c8f1844eb5ff950aca309
Anthony Cabulang <zyxanthony15@gmail.com>
2023-11-19 22:05:41 +0800

    Changing the operator

--- conditional-and-iterations/for-loop.js
+++ conditional-and-iterations/for-loop.js
@@ -10,7 +10,7 @@ document.write("<p>Print the sum of all even numbers from 1-10</p>")
     for (i = 0; i <= num; i++) {
         document.write(" " + i);
        if (i % 2 == 0) {
-           sum = sum + i;
+           sum += i;
         }
     }
     document.write("<br><br><p>The sum of even numbers from 1 to 10: </p>" + sum);

4eb0bbf505f2398ece6714985abd4ed1df2592f2
Anthony Cabulang <zyxanthony15@gmail.com>
2023-11-19 20:59:32 +0800

    Adding conditional folder and its content

---
+++ conditional-statements/if-else.js
@@ -0,0 +1,8 @@
+let grade = parseInt(prompt("Type your grade here: "));
+if (grade <= 100 && grade >= 90) {
+    document.write("You have an A grade, Congrats!");
+}   else if (grade <=89 && grade >= 79) {
+    document.write("You have a B grade, Congrats!");
+}   else {
+    document.write("You have F grade, Congrats!");
+    }

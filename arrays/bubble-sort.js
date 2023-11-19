56c9788a440542889e4a1e1d2ec84a16b3e4c1fd
Anthony Cabulang <zyxanthony15@gmail.com>
2023-11-19 19:39:38 +0800

    Adding bubble sort and modified create-array.html

---
+++ arrays/bubble-sort.js
@@ -0,0 +1,8 @@
+let arrangeMe = [10, 32, 402, 21, 11, 103, 23, 1, 5, 95];
+let i, j, temp;
+
+document.write("<p> Printing out the numbers without arranging </p>"); 
+for (i = 0; i < arrangeMe.length; i++) {
+    document.write(arrangeMe[i] + ",  ");
+
+}

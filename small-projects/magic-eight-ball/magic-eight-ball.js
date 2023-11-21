0e7d887584a7569351dfb4ea277cc87483666df0
Anthony Cabulang <zyxanthony15@gmail.com>
2023-11-22 05:32:22 +0800

    Task finished

--- small-projects/magic-eight-ball/magic-eight-ball.js
+++ small-projects/magic-eight-ball/magic-eight-ball.js
@@ -3,7 +3,7 @@ userName ? document.write(`Hello, ${userName}!`) :
                 console.log("Hello");
 
 let userQuestion = prompt("Enter your question: ");
-document.write(`${userQuestion}, ${userName}?`);
+document.write(`<br> Your Question: ${userQuestion}?, by  ${userName} <br>`);
 
 let randomNumber = Math.floor(Math.random() * 8);
 let eightBall = "";
@@ -11,27 +11,27 @@ eightBall = randomNumber;
 //Practicing control flow using switch and if else, they have the same algorithm tho for practice
 // Switch Statement
 switch (eightBall) {
-  case 0 : document.write("It is certain");
+  case 0 : document.write("Your Answer: It is certain");
   break;
-  case 1 : document.write("It is decidedly so");
+  case 1 : document.write("Your Answer: It is decidedly so");
   break;
-  case 2 : document.write("Reply hazy try again");
+  case 2 : document.write("Your Answer: Reply hazy try again");
   break;
-  case 3 : document.write("Cannot predict now");
+  case 3 : document.write("Your Answer: Cannot predict now");
   break;
-  case 4 : document.write("Do not count on it");
+  case 4 : document.write("Your Answer: Do not count on it");
   break;
-  case 5 : document.write("My sources say no");
+  case 5 : document.write("Your Answer: My sources say no");
   break;
-  case 6 : document.write("Outlook not so good");
+  case 6 : document.write("Your Answer: Outlook not so good");
   break;
-  case 7 : document.write("Signs point to yes");
+  case 7 : document.write("Your Answer: Signs point to yes");
   break;
-  default : document.write("It is what it is");
+  default : document.write("Your Answer: It is what it is");
   break;
 } 
 //If-Else Statement     
-/*
+
 if (eightBall === 0) {
   console.log("It is certain");
 } else if (eightBall === 1) {
@@ -52,4 +52,3 @@ if (eightBall === 0) {
   console.log("It is what it is");
 }
 
-*/

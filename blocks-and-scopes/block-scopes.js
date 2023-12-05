5ebec93b8e8dd48b901d80d05f8b8c5c31af2834
Anthony Cabulang <zyxanthony15@gmail.com>
2023-12-05 23:02:28 +0800

    Adding content in block-scopes.js

---
+++ blocks-and-scopes/block-scopes.js
@@ -0,0 +1,7 @@
+const city = "New York City"
+function logCitySkyline() {
+  let skyscraper  = "Empire State Building"
+  return 'The stars over the ' + skyscraper + ' in ' + city;
+}
+
+console.log(logCitySkyline());

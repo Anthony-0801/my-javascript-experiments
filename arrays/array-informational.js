6cfcacaec964fab17c73746d18fe5b069f8e1366
Anthony Cabulang <zyxanthony15@gmail.com>
2023-12-06 23:45:55 +0800

    Adding how to create and how to access arrays

--- arrays/array-informational.js
+++ arrays/array-informational.js
@@ -1,1 +1,14 @@
-//to be updated later 
+//Creating an array
+let newYearsResolutions = ['Keep a journal', 'Take a falconry class', 'Learn to juggle'];
+console.log(newYearsResolutions);
+
+const hobbies = ['biking', 'reading', 'playing video games'];
+console.log(hobbies);
+
+//Accessing the content or an index of an array
+const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.'];
+
+let listItem = famousSayings[0];
+console.log(listItem);
+console.log(famousSayings[2]);
+console.log(famousSayings[3]); //undefined

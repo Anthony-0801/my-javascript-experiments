1d5c854b146992a93cb47e63a24c1bd4b74b3403
Anthony Cabulang <zyxanthony15@gmail.com>
2023-12-08 00:54:20 +0800

    adding array-and-functions file

---
+++ arrays/arrays-and-functions.js
@@ -0,0 +1,11 @@
+const flowers = ['peony', 'daffodil', 'marigold'];
+
+function addFlower(arr) {
+  arr.push('lily');
+}
+
+addFlower(flowers);
+
+console.log(flowers); // Output: ['peony', 'daffodil', 'marigold', 'lily']
+
+//another array and functions

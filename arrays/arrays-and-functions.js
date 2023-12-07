20ab2b5f01e6ccfb2c21bb259360c79d0aaa9cf1
Anthony Cabulang <zyxanthony15@gmail.com>
2023-12-08 01:17:34 +0800

    Adding content

--- arrays/arrays-and-functions.js
+++ arrays/arrays-and-functions.js
@@ -9,3 +9,18 @@ addFlower(flowers);
 console.log(flowers); // Output: ['peony', 'daffodil', 'marigold', 'lily']
 
 //another array and functions
+const concept = ['arrays', 'can', 'be', 'mutated'];
+
+function changeArr(arr){
+  arr[3] = 'MUTATED';
+}
+
+changeArr(concept);
+console.log(concept); //[ 'arrays', 'can', 'be', 'MUTATED' ]
+
+function removeElement(newArr) {
+  newArr.pop();
+}
+
+removeElement(concept);
+console.log(concept); //[ 'arrays', 'can', 'be' ]

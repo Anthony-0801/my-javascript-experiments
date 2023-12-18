5d402eb8fc79c7e4cbafe69abaacc8bf451bf2a5
Anthony Cabulang <zyxanthony15@gmail.com>
2023-12-19 00:35:03 +0800

    Finishing contents of loop.js

--- loops/for-loop.js
+++ loops/for-loop.js
@@ -47,4 +47,17 @@ do {
   console.log(cupsAdded + " cups was added.")
 } while (cupsAdded < cupsOfSugarNeeded)
 
+//using break to stop a loop from printing names in the rapper list
+const rapperArray = ["Lil' Kim", "Jay-Z", "Notorious B.I.G.", "Tupac"];
+
+// Write your code below
+for (let i = 0; i < rapperArray.length; i++){
+  console.log(rapperArray[i]);
+  if (rapperArray[i] === "Notorious B.I.G.") {
+    console.log("Ooops let's stop! ");
+    break;
+  }
+}
+
+console.log("And if you don't know, now you know.");
 

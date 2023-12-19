5c2d08d8a0323786dcb717631944a175690d5d4b
Anthony Cabulang <zyxanthony15@gmail.com>
2023-12-19 23:56:37 +0800

    adding content

--- higher-order-functions/high-functions.js
+++ higher-order-functions/high-functions.js
@@ -12,3 +12,22 @@ const checkThatTwoPlusTwoEqualsFourAMillionTimes = () => {
   isTwoPlusTwo();
   console.log(isTwoPlusTwo.name);
   
+//Functions as a Parameter, assigning a function as a parameter of another function
+const addTwo = num => {
+    return num + 2;
+  }
+  
+  const checkConsistentOutput = (func, val) => {
+    let checkA = val + 2;
+    let checkB= func(val);
+  
+    if (checkA === checkB) {
+      return checkB;
+    } else {
+      console.log('inconsistent results.');
+    }
+  }
+  
+  console.log(checkConsistentOutput(addTwo, 5));
+
+

f35967a6e809388d491de1e72c7666319c49cfbc
Anthony Cabulang <zyxanthony15@gmail.com>
2023-12-20 13:06:19 +0800

    Adding subfolder and its file

---
+++ iterators/iterators.js
@@ -0,0 +1,25 @@
+//.forEach() method
+const artists = ['Picasso', 'Kahlo', 'Matisse', 'Utamaro'];
+
+artists.forEach(artist => {
+  console.log(artist + ' is one of my favorite artists.');
+});
+
+//.map() method
+const numbers = [1, 2, 3, 4, 5];
+
+const squareNumbers = numbers.map(number => {
+  return number * number;
+});
+
+console.log(squareNumbers);
+
+//.filter() method
+const things = ['desk', 'chair', 5, 'backpack', 3.14, 100];
+
+const onlyNumbers = things.filter(thing => {
+  return typeof thing === 'number';
+});
+
+console.log(onlyNumbers);
+

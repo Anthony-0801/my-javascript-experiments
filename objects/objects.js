c0d0f03db79ad5c2ffdecd559a33986b3a9a6544
Anthony Cabulang <zyxanthony15@gmail.com>
2023-12-22 00:07:15 +0800

    adding content

--- objects/objects.js
+++ objects/objects.js
@@ -106,3 +106,25 @@ spaceship.passengers = [
 
 let firstPassenger = spacceship.passengers[0];
 console.log(firstPassenger);
+
+
+//Pass by Reference
+//Objects are passed by reference. This means when we pass a variable assigned to an object into a function as an argument,
+//the computer interprets the parameter name as pointing to the space in memory holding that object. 
+//As a result, functions which change object properties actually mutate the object permanently (even when the object is assigned to a const variable).
+let spaceship = {
+  'Fuel Type' : 'Turbo Fuel',
+  homePlanet : 'Earth'
+};
+
+let greenEnergy = object => {
+  object['Fuel Type'] = 'avocado oil'
+}
+
+let remotelyDisable = objec => {
+  objec.disabled = true;
+}
+
+greenEnergy(spaceship);
+remotelyDisable(spaceship);
+console.log(spaceship);

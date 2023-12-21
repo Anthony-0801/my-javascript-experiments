f57d23999b252412e6c9d1f63dd83898273c854c
Anthony Cabulang <zyxanthony15@gmail.com>
2023-12-21 23:29:07 +0800

    fixing and adding some contents

--- objects/objects.js
+++ objects/objects.js
@@ -34,14 +34,34 @@ let isActive = spacesship['Active Mission'];
 console.log(isActive);
 
 //using assignment operator = 
-let spaceship = {
+let spaceeship = {
   'Fuel Type' : 'Turbo Fuel',
   homePlanet : 'Earth',
   color: 'silver',
   'Secret Mission' : 'Discover life outside of Earth.'
 };
 
-spaceship.color = 'glorious gold';
-spaceship.numEngines = 1,2,3,4,5,6,7,8,9,10;
-delete spaceship['Secret Mission'];
-console.log(spaceship);
+spaceeship.color = 'glorious gold';
+spaceeship.numEngines = 1,2,3,4,5,6,7,8,9,10;
+delete spaceeship['Secret Mission'];
+console.log(spaceeship);
+
+
+//object methods
+//When the data stored on an object is a function we call that a method. A property is what an object has, while a method is what an object does.
+let retreatMessage = 'We no longer wish to conquer your planet. It is full of dogs, which we do not care for.';
+
+//set some methods
+let alienShip = {
+  retreat () {
+    console.log(retreatMessage);
+},
+
+  takeOff() {
+    console.log("Spim... Borp... Glix... Blastoff!");
+  }
+};
+
+//call the set methods
+alienShip.retreat();
+alienShip.takeOff();

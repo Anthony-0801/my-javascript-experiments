9b60f71eae2286fe46f40de58cfb80513ce30d00
Anthony Cabulang <zyxanthony15@gmail.com>
2023-12-21 23:47:00 +0800

    adding content

--- objects/objects.js
+++ objects/objects.js
@@ -65,3 +65,44 @@ let alienShip = {
 //call the set methods
 alienShip.retreat();
 alienShip.takeOff();
+
+//accesssing in nested objects
+let spacceship = {
+  passengers: null,
+  telescope: {
+    yearBuilt: 2018,
+    model: "91031-XLT",
+    focalLength: 2032 
+  },
+  crew: {
+    captain: { 
+      name: 'Sandra', 
+      degree: 'Computer Engineering', 
+      encourageTeam() { console.log('We got this!') },
+     'favorite foods': ['cookies', 'cakes', 'candy', 'spinach'] }
+  },
+  engine: {
+    model: "Nimbus2000"
+  },
+  nanoelectronics: {
+    computer: {
+      terabytes: 100,
+      monitors: "HD"
+    },
+    'back-up': {
+      battery: "Lithium",
+      terabytes: 50
+    }
+  }
+}; 
+//accessing the spaceship nested object and assigning it into a variable
+let capFave = spacceship.crew.captain['favorite foods'][0];
+console.log(capFave);
+spaceship.passengers = [
+  {
+    name: 'Anthony'
+  }
+]
+
+let firstPassenger = spacceship.passengers[0];
+console.log(firstPassenger);

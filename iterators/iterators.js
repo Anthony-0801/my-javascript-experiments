063a19acc5d48526eaa65c4cf9461a45dd40e3a1
Anthony Cabulang <zyxanthony15@gmail.com>
2023-12-21 22:26:33 +0800

    adding final contents

--- iterators/iterators.js
+++ iterators/iterators.js
@@ -114,3 +114,28 @@ const interestingWords = worrds.filter(woord => {return woord.length > 5;});
 console.log(interestingWords);
 
 console.log(interestingWords.every((word) => { return word.length > 5} ));
+
+// more random iterator method examples
+const cities = ['Orlando', 'Dubai', 'Edinburgh', 'Chennai', 'Accra', 'Denver', 'Eskisehir', 'Medellin', 'Yokohama'];
+
+const nums = [1, 50, 75, 200, 350, 525, 1000];
+
+//  Choose a method that will return undefined
+cities.forEach(city => console.log('Have you visited ' + city + '?'));
+
+// Choose a method that will return a new array
+const longCities = cities.filter(city => city.length > 7);
+
+// Choose a method that will return a single value
+const word = cities.reduce((acc, currVal) => {
+  return acc + currVal[0]
+}, "C");
+
+console.log(word)
+
+// Choose a method that will return a new array
+const smallerNums = nums.map(num => num - 5);
+
+// Choose a method that will return a boolean value
+nums.some(num => num < 0);
+

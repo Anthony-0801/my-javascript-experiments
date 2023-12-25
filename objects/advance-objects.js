b93d8b7ece38c230dcda70202f5699adefbb8cc8
Anthony Cabulang <zyxanthony15@gmail.com>
2023-12-25 23:03:33 +0800

    Adding advance objects and some content of it

---
+++ objects/advance-objects.js
@@ -0,0 +1,24 @@
+// The this keyword references the calling object which provides access to the calling object’s properties.
+const goat = {
+    dietType: 'herbivore',
+    makeSound() {
+      console.log('baaa');
+    },
+    diet() {
+      console.log(this.dietType);
+    }
+  };
+  
+  goat.diet(); 
+  // Output: herbivore
+  
+  // another example
+  const robot = {
+    model: '1E78V2',
+    energyLevel: 100,
+    provideInfo() {
+      return `I am ${this.model} and my current energy level is ${this.energyLevel}.`
+    }
+  };
+  
+  console.log(robot.provideInfo());

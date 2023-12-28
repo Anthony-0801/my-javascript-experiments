a664405df582ecec660cca3beec7bf95f56c8b7d
Anthony Cabulang <zyxanthony15@gmail.com>
2023-12-28 23:52:22 +0800

    adding challenge 2

--- arrays/array-challenges.js
+++ arrays/array-challenges.js
@@ -6,3 +6,12 @@ for (num = 1; num <= 20; num++) {
     console.log(num);
 };
 }
+
+function printOdd3to20 () {
+    let num;
+for (num = 3; num <=20; num++) {
+    if(num % 2 !== 0) {
+        console.log(num);
+    }
+};
+}

284e811a141b3dae5013513a16b39256aa135e5a
Anthony <zyxanthony15@gmail.com>
2024-02-07 02:20:11 +0800

    updating its content

--- random/intro - algorithm 4 part 2.js
+++ random/intro - algorithm 4 part 2.js
@@ -12,3 +12,31 @@ function printAverage(x){
    
  y = printAverage([2,5,8]);
  console.log(y); // should log 5
+
+
+
+//Create an array with all the odd integers between 1 and 255 (inclusive)
+function returnOddArray(){
+    var arr = [];
+    for(var i = 1; i <= 255; i += 2){
+        arr.push(i);
+    }
+    return arr;
+ }
+ y = returnOddArray();
+ console.log(y); // should log [1,3,5,...,253,255]
+
+
+
+//Square each value in a given array, returning that same array with changed values.
+function squareValue(x){
+    for(var i = 0; i < x.length; i++){
+        x[i] = x[i] * x[i];
+    }
+    return x;
+ }
+ y = squareValue([1,2,3]);
+ console.log(y); // should log [1,4,9]
+   
+ y = squareValue([2,5,8]);
+ console.log(y); // should log [4,25,64]

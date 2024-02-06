586d135c844bc8eeef34123a9e3f5855199d767c
Anthony <zyxanthony15@gmail.com>
2024-02-07 00:51:00 +0800

    adding updates

--- random/intro - algorithm 3 part 2.js
+++ random/intro - algorithm 3 part 2.js
@@ -13,7 +13,18 @@ function printUpTo(x){
 
 
 
-
+//PrintSum
+function printSum(x){
+    var sum = 0;
+    for(var i = 0; i <= x; i++){
+      console.log(i);
+      sum += i;
+      console.log(sum);
+    }
+    return sum
+  }
+  y = printSum(255) // should print all the integers from 0 to 255 and with each integer print the sum so far.
+  console.log(y) // should print 32640
 
 
 

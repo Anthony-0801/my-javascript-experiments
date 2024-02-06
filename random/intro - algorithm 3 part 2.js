08b1fe3096a3f1280024ac9a81a4c954625abdab
Anthony <zyxanthony15@gmail.com>
2024-02-07 00:51:35 +0800

    adding changes

--- random/intro - algorithm 3 part 2.js
+++ random/intro - algorithm 3 part 2.js
@@ -28,3 +28,13 @@ function printSum(x){
 
 
 
+
+//PrintSumArray
+function printSumArray(x){
+    var sum = 0;
+    for(var i=0; i<x.length; i++) {
+      sum += x[i];
+    }
+    return sum;
+  }
+  console.log( printSumArray([1,2,3]) ); // should log 6

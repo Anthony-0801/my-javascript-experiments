f8f86b84e8dc15d44d2f0e8e0725fdd5f961f6fa
Anthony <zyxanthony15@gmail.com>
2024-05-13 23:43:19 +0800

    experimenting on variable types

--- mslearn/exercise-1/app.js
+++ mslearn/exercise-1/app.js
@@ -1,3 +1,7 @@
-let startingPokerChips = 100; // points
-let players = 3;
-let noOfStarterCards = 2;
+const STARTING_POKER_CHIPS = 100; // points
+const PLAYERS = 3;
+const NO_OF_STARTER_CARDS = 2;
+
+let playerOnePoints = STARTING_POKER_CHIPS;
+let playerTwoPoints = STARTING_POKER_CHIPS;
+let playerThreePoints = STARTING_POKER_CHIPS;

34a0b27741fe89ffebaac302e81f69eb3025d9b0
Anthony <zyxanthony15@gmail.com>
2024-05-26 09:33:16 +0800

    adding mschallenge

---
+++ mslearn/mschallenge-2/app.js
@@ -0,0 +1,9 @@
+let iceCreamFlavors = [
+    { name: "Chocolate", type: "Chocolate", price: 2 }, 
+    { name: "Strawberry", type: "Fruit", price: 1 }, 
+    { name: "Vanilla", type: "Vanilla", price: 2 }, 
+    { name: "Pistachio", type: "Nuts", price: 1.5 }, 
+    { name: "Neapolitan", type: "Chocolate", price: 2}, 
+    { name: "Mint Chip", type: "Chocolate", price: 1.5 },
+    { name: "Raspberry", type: "Fruit", price: 1},
+   ];   

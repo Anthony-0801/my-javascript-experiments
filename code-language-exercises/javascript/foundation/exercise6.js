660ec4c1a441675218b9183f2c87908b2cdd2264
Anthony <zyxanthony15@gmail.com>
2024-07-21 18:54:27 +0800

    adding a simple exercise

---
+++ code-language-exercises/javascript/foundation/exercise6.js
@@ -0,0 +1,23 @@
+let user = [Number(prompt('Enter the first number (X): ')), Number(prompt('Enter the second number (Y): '))];
+let operation = prompt('Enter the operation: ');
+
+if(typeof user[0] !== 'number' || typeof user[1] !== 'number') {
+    document.write('Invalid input');
+}
+
+if(operation === 'a' || 'A') {
+    document.write(`You've used addition the output of ${user[0]} and ${user[1]} is: ` + user[0] + user[1]);
+} else if(operation === 's' || 'S') {
+    document.write(`You've used subtraction the output of ${user[0]} and ${user[1]} is: ` + user[0] - user[1]);
+} else if(operation === 'm' || 'M') {
+    document.write(`You've used multiplication the output of ${user[0]} and ${user[1]} is: ` + user[0] * user[1]);
+} else if(operation === 'd' || 'D') {
+    document.write(`You've used division the output of ${user[0]} and ${user[1]} is: ` + user[0] / user[1]);
+} else if(operation === 'r' || 'R') {
+    document.write(`You've used remainder the output of ${user[0]} and ${user[1]} is: ` + user[0] % user[1]);
+} else if(operation === 'e' || 'E') {
+    document.write(`You've used exponent the output of ${user[0]} and ${user[1]} is: ` + user[0] ** user[1]);
+} else {
+    document.write('Invalid operation');
+}
+
